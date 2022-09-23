3d1c71f5d85f6f71b144200c8c48c39875c2e214
Juan Carlos Cedeño <juanka_cd95@hotmail.com>
2022-09-23 17:30:10 -0500

    Add second levels dir into definitions.py

--- src/utils/definitions.py
+++ src/utils/definitions.py
@@ -13,3 +13,7 @@ import os
 SRC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 ROOT_DIR = os.path.abspath(os.path.join(SRC_DIR, os.pardir))
 DATA_DIR = os.path.join(ROOT_DIR, "data")
+
+# Second level dirs
+UNPROCESSED_DATA_DIR = os.path.join(DATA_DIR, "unprocessed")
+PROCESSED_DATA_DIR = os.path.join(DATA_DIR, "processed")
