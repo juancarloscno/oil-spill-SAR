4fe63086f9c9e35ccba9447bc5ffe7683294dfd0
Juan Carlos Cedeño <juanka_cd95@hotmail.com>
2022-09-25 14:57:53 -0500

    Add copyright to download_sentinel_1.sh

---
+++ scripts/download_sentinel_1.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Downloading Workflow Script
+# This script downloads all Sentinel-1 SAR images used in the project.
+# This dataset consists of almost 10000 images along Ecuadorian Exclusive Economic Zone, each of ~1GB.
+# Copyright (c) 2022 Juan Carlos Cedeño
+# Licensed under the MIT License (see LICENSE for details)
+# Written by Juan Carlos Cedeño
