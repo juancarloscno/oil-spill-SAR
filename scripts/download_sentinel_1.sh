5d77abc843d271e8756e600498af45c094911378
Juan Carlos Cedeño <juanka_cd95@hotmail.com>
2022-09-30 08:06:30 +0000

    Update header description of download_sentinel_1.sh

--- scripts/download_sentinel_1.sh
+++ scripts/download_sentinel_1.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# Downloading Workflow Script
-# This script downloads all Sentinel-1 SAR images used in the project.
+# Downloading Workflow: Script to download all Sentinel-1 SAR images used in the project.
 # This dataset consists of almost 10000 images along Ecuadorian Exclusive Economic Zone, each of ~1GB.
-# Copyright (c) 2022 Juan Carlos Cedeño
-# Licensed under the MIT License (see LICENSE for details)
-# Written by Juan Carlos Cedeño
+
+# Copyright (c) 2022 Juan Carlos Cedeño Noblecilla
+
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+# Written by Juan Carlos Cedeño Noblecilla.
 
 # Read dotenv located in the root of the project
 set -a
