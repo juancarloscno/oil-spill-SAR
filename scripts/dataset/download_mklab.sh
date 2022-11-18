e30b686a6665d0cc74878541042ba6ffbee725af
juan.cedenon <juan.cedenon@dgx-node-0-0.cedia.edu.ec>
2022-11-18 05:43:20 +0000

    Add MKLab Dataset downloader script

---
+++ scripts/dataset/download_mklab.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Downloading Workflow: Script to download all Oil Spill Detection Dataset (MKLab) images used in the project.
+# This dataset consists of almost 1002 images of oil spills ocurred in Europeam Oceans
+
+# Copyright (c) 2022 Juan Carlos Cedeño Noblecilla
+
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
+# Written by Juan Carlos Cedeño Noblecilla.
+
+python ./src/data/dataset/mklab.py
