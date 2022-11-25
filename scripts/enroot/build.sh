f92847a2ed7b79d08de9b9463b201f0286d8c4c3
Juan Carlos Cedeño <juanka_cd95@hotmail.com>
2022-11-25 13:19:04 -0500

    Add Sentinel Application Platform v8.0 software install procedure to build the container

--- scripts/enroot/build.sh
+++ scripts/enroot/build.sh
@@ -14,9 +14,12 @@ COCO_API_VERSION=v0.6.0
 PLATFORM=tensorflow
 TF_VERSION_FLAG=tf2
 PROJECT=oil-spill-SAR
+WORK_DIR="/workspace/${PROJECT}"
 SQSH_FILENAME=oil-spill-sar+tf2+py3.sqsh
 ROOT_DIR="${HOME}/${PROJECT}"
 SQSH_FILE="${ROOT_DIR}/${SQSH_FILENAME}"
+ESA_SNAP="esa-snap_sentinel_unix_8_0.sh"
+ESA_SNAP_LINK="https://download.esa.int/step/snap/8.0/installers/${ESA_SNAP}"
 
 # Delete any SQSH file locate in the root directory
 if test -f "$SQSH_FILE"; then
@@ -24,10 +27,19 @@ if test -f "$SQSH_FILE"; then
     rm "$SQSH_FILE"
 fi
 # Build an SQSH image of Tensorflow from NVIDIA NGC (Repository for NVIDIA Optimized Frameworks)
-# Release 20.09 is based on CUDA 11.0.3, NVIDIA driver release 450.51
+# Release 20.09 is based on CUDA 11.4, which requires NVIDIA driver release 470 or later
 # compatible with NVIDIA GPUs of CEDIA's HPC
-enroot import --output "$SQSH_FILE" docker://nvcr.io#nvidia/${PLATFORM}:${NVIDIA_RELEASE}-${TF_VERSION_FLAG}-${PYTHON_VERSION_FLAG}
+printf "\nBuilding an SQSH image of Tensorflow from NVIDIA NGC (Repository for NVIDIA Optimized Frameworks)...\n"
+enroot import --output "$SQSH_FILE" "docker://nvcr.io#nvidia/${PLATFORM}:${NVIDIA_RELEASE}-${TF_VERSION_FLAG}-${PYTHON_VERSION_FLAG}"
 # Create an container rootfs (RF) from SQSH file downloaded
+printf "\nCreating an container rootfs (RF) from SQSH file downloaded...\n"
 enroot create --force --name $PROJECT "$SQSH_FILE"
+# Update and install packages
+printf "\nUpdating and installing packages in the container...\n"
+enroot start --root --rw $PROJECT sh -c 'apt update -y && apt install gdal-bin libsqlite3-mod-spatialite -y'
+# Install Sentinel Application Platform (SNAP) v8
+printf "\nInstalling Sentinel Application Platform (SNAP) v8 in the container...\n"
+enroot start --rw --mount "$PWD":$WORK_DIR $PROJECT sh -c "wget -O ${ESA_SNAP} ${ESA_SNAP_LINK} && chmod +x ${ESA_SNAP} && ./${ESA_SNAP} -q -varfile ${WORK_DIR}/esa-snap_install_unix_8_0.varfile"
 # Install python dependencies
-enroot start --rw --mount "$HOME"/oil-spill-SAR:"$HOME"/oil-spill-SAR "$PROJECT" pip --no-cache-dir install -r "$HOME"/oil-spill-SAR/requirements.txt
+printf "\nInstalling python dependencies in the container...\n"
+enroot start --rw --mount "$PWD":$WORK_DIR $PROJECT sh -c "cd ${WORK_DIR} && pip --no-cache-dir install -r requirements.txt"
