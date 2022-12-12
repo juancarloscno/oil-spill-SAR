7cd42cc68c2160889fc780d9c6f019cdba067454
Juan Carlos Cedeño <juanka_cd95@hotmail.com>
2022-12-12 17:31:52 +0000

    Update SNAP modules, move the JPY directory under /opt/snap, and install snappy and jpy modules locally

--- scripts/snap/build.sh
+++ scripts/snap/build.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # SNAP Workflow: Script to install ESA SNAP v8 in the container
 
 # Copyright (c) 2022 Juan Carlos Cedeño Noblecilla
@@ -15,16 +16,22 @@ ESA_SNAP_PKG='esa-snap_sentinel_unix_8_0.sh'
 ESA_SNAP_LINK="https://download.esa.int/step/snap/8.0/installers/${ESA_SNAP_PKG}"
 SNAP_DIR="/opt/snap"
 SNAPPY_DIR="${SNAP_DIR}/snappy"
+JPY_DIR="${SNAP_DIR}/jpy"
 JPY_REPO="https://github.com/bcdev/jpy.git"
 PYTHON_DIR="/usr/bin/python"
 
 # Download the SNAP installer from the ESA website in temporary folder and execute the installation using the varfile
-wget -O /tmp/${ESA_SNAP_PKG} ${ESA_SNAP_LINK}
-chmod +x /tmp/${ESA_SNAP_PKG} && /tmp/${ESA_SNAP_PKG} -q -varfile $ESA_SNAP_VARFILE_FILEPATH
+if [ ! -f "/tmp/${ESA_SNAP_PKG}" ]; then
+	echo -e "\nGetting SNAP v.8.0 installer from ESA website...\n"
+	wget -O /tmp/${ESA_SNAP_PKG} ${ESA_SNAP_LINK}
+	chmod +x /tmp/${ESA_SNAP_PKG} && /tmp/${ESA_SNAP_PKG} -q -varfile $ESA_SNAP_VARFILE_FILEPATH
+fi
 
 # Export variables related with JAVA
+echo -e "\nExporting JAVA environment variables\n"
 export JDK_HOME="/usr/lib/jvm/java-1.8.0-openjdk-amd64"
-export JAVA_HOME=$JDK_HOME
+export JAVA_HOME="${JDK_HOME}"
+export LD_LIBRARY_PATH="${JDK_HOME}/jre/lib/server:${LD_LIBRARY_PATH}"
 # Set JAVA_HOME in the SNAP configuration file, permanently. Otherwise, JAVA_HOME will be set each time the container is started
 sed -i "s+jdkhome=.*+jdkhome=$JAVA_HOME+g" /opt/snap/etc/snap.conf
 
@@ -33,31 +40,38 @@ if [ ! -d $SNAPPY_DIR ]; then
     mkdir -p $SNAPPY_DIR
 fi
 
-# Move to the snappy folder
-cd $SNAPPY_DIR
-
 # Download the JPY repository from the GitHub and build its wheels
+cd $SNAP_DIR
 git clone $JPY_REPO
 cd jpy
-python setup.py bdist_wheel
+pip install --upgrade pip wheel
+python setup.py build maven bdist_wheel
 
 # Find wheel of JPY
 JPY_WHEEL=$(find dist -name "*.whl")
 JPY_WHEEL_FILENAME=$(basename "$JPY_WHEEL")
 # Move JPY wheel to snappy folder
 cp "$JPY_WHEEL" "${SNAPPY_DIR}/${JPY_WHEEL_FILENAME}"
-# Remove JPY folder
-rm -rf "${SNAPPY_DIR}/jpy"
-
-# Install Snapista library
-pip install --use-pep517 git+https://github.com/snap-contrib/snapista.git
-pip install lxml
 
 # Launch snappy-conf to configure snappy for Python 3.8
+echo -e "\nInstalling The Sentinel Application Platform (SNAP)...\n"
 $SNAP_DIR/bin/snappy-conf $PYTHON_DIR $SNAP_DIR
+echo -e "\nSNAP has been installed successfully!\n"
 
-# Install Snappy locally
+# Updating SNAP
+echo -e "\nUpdating SNAP modules..."
+$SNAP/bin/snap --nosplash --nogui --modules --update-all
+echo -e "\nSNAP modules has been updated sucessfully!\n"
+
+# Install Snappy and JPY locally
 cd $SNAPPY_DIR && python setup.py install
+cd $JPY_DIR && python setup.py install
+
+# Install Snapista
+pip install lxml
+pip install --use-pep517 git+https://github.com/snap-contrib/snapista.git
 
 # Add GPT to the PATH, permanently, because Snapista read 'GPT' trhough $PATH variable
 echo -e '\nPATH="/opt/snap/bin":$PATH' >> $HOME/.profile
+echo -e "\nJDK_HOME="$JDK_HOME"" >> $HOME/.profile
+echo -e "\nJAVA_HOME="$JAVA_HOME"" >> $HOME/.profile
